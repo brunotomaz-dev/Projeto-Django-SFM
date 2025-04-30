2d7f88e8db12a8729b82b5e974058832fe88c884
brunotomaz-dev <tomaz.bruno@gmail.com>
2025-04-30 07:17:06 -0300

    🔧 Refatora a classe ServiceOrderViewSet para corrigir referências de campos e melhorar a consulta SQL.

--- backend/myapp/apps.py
+++ backend/myapp/apps.py
@@ -17,18 +17,18 @@ class MyappConfig(AppConfig):
     name = "myapp"
     verbose_name = "API de Coleta de Dados de Máquinas"
 
-    def ready(self):
-        """
-        Função chamada quando o aplicativo está pronto.
-        """
-        if all(
-            [
-                "runserver" in sys.argv,  # Apenas no runserver
-                Path(sys.argv[0]).stem != "pytest",  # Não durante testes
-                os.environ.get("RUN_MAIN", None) == "true",  # Apenas no processo principal
-            ]
-        ):
-            # pylint: disable=import-outside-toplevel
-            from .schedulers import start_scheduler
+    # def ready(self):
+    #     """
+    #     Função chamada quando o aplicativo está pronto.
+    #     """
+    #     if all(
+    #         [
+    #             "runserver" in sys.argv,  # Apenas no runserver
+    #             Path(sys.argv[0]).stem != "pytest",  # Não durante testes
+    #             os.environ.get("RUN_MAIN", None) == "true",  # Apenas no processo principal
+    #         ]
+    #     ):
+    #         # pylint: disable=import-outside-toplevel
+    #         from .schedulers import start_scheduler
 
-            threading.Thread(target=start_scheduler).start()
+    #         threading.Thread(target=start_scheduler).start()
